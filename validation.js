1a955167ebcad0eb184f69a0f66075de6b3a474a
Christopher <98278053+cjmcs@users.noreply.github.com>
2022-03-18 17:08:20 +0000

    Added initial validation to html form via js regex

--- validation.js
+++ validation.js
@@ -1,1 +1,31 @@
-// validation script here
+
+
+// /first way to write regex
+// let regex = /[a-z] /gi;
+
+// // second way to write regex
+// let regex2 = new RegExp(/[a-z]/, 'i');
+
+
+// // validation script here
+const inputs = document.querySelectorAll('input');
+
+const patterns = {
+    telephone: /^[0-9]{11}$/
+}
+// // Appends Class name to input type if valid against regex
+function validate(field, regex){
+//    console.log(regex.test(field.value));
+if(regex.test(field.value)){
+    field.className = 'valid';
+} else {
+    field.className = 'invalid';
+}
+}
+
+inputs.forEach((input) => {
+        input.addEventListener('keyup', (e) => {
+            // console.log(e.target.attributes.name.value);
+            validate(e.target, patterns[e.target.attributes.name.value])
+        });
+    });

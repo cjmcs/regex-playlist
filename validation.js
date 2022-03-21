0235038479618c59f58b4557a2ee2a534c184ffb
Christopher <98278053+cjmcs@users.noreply.github.com>
2022-03-21 16:32:02 +0000

    Added final regex validations

--- validation.js
+++ validation.js
@@ -11,9 +11,14 @@
 const inputs = document.querySelectorAll('input');
 
 const patterns = {
-    telephone: /^[0-9]{11}$/
+        telephone: /^[0-9]{11}$/
+    ,   username: /^[a-z\d]{5,12}$/i       //AplaNumeric between 5-12
+    ,   password: /^[-@a-z\d_]{8,20}$/i   //Alpha Numeric icluding hyphen underscore and @
+    ,   slug:     /^[-a-z\d]{8,20}$/i    //AplaNumeric between 8-20}
+    ,   email:     /^([a-z\d\._-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/   //email
 }
-// // Appends Class name to input type if valid against regex
+
+    // // Appends Class name to input type if valid against regex
 function validate(field, regex){
 //    console.log(regex.test(field.value));
 if(regex.test(field.value)){
@@ -22,10 +27,10 @@ if(regex.test(field.value)){
     field.className = 'invalid';
 }
 }
-
+// Runs validate Function
 inputs.forEach((input) => {
         input.addEventListener('keyup', (e) => {
-            // console.log(e.target.attributes.name.value);
+            console.log(e.target.attributes.name.value);
             validate(e.target, patterns[e.target.attributes.name.value])
         });
     });
